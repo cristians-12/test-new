fd3fffbf4575bcefe1bbfcc0de859b74807f4e6d
cristians-12 <cristian.sanchez.casd@gmail.com>
2026-07-10 13:55:18 -0500

    feat: implement CustomBackButton and CustomSearchHeader components; update styles and remove unused theme file

---
+++ frontend/src/components/organisms/custom-search-header/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { colors } from '../../../utils/colors';
+
+export default function CustomSearchHeader() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Bienvenido</Text>
+            <View style={styles.inputWrapper}>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Buscar productos...'
+                    placeholderTextColor={colors.darkGray}
+                />
+                <TouchableOpacity>
+                    <Icon name="search" size={18} color={colors.darkGray} style={styles.inputIcon} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
--- frontend/src/components/templates/home/index.tsx
+++ frontend/src/components/templates/home/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { styles } from './styles';
+import CustomSearchHeader from '../../organisms/custom-search-header';
 
 export default function HomeTemplate() {
     return (
         <View>
-            <Text style={styles.text}>Productos:</Text>
+            <CustomSearchHeader />
         </View>
     );
 }
