a2021be84ff34e980a7c1c94c5c95efb9e60f472
cristians-12 <cristian.sanchez.casd@gmail.com>
2026-07-11 16:26:30 -0500

    feat: implement ProductCard component with styles and image handling

--- frontend/src/components/molecules/product-card/index.tsx
+++ frontend/src/components/molecules/product-card/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Product } from '../../../types';
+import { styles } from './styles';
+import ImageComponent from '../image-component';
+import { images } from '../../../assets';
 
-export default function ProductCard() {
+interface Props {
+    product: Product;
+}
+
+
+export default function ProductCard({ product }: Props) {
+
+    const { name, price, image_url, stock } = product;
+
     return (
-        <View>
-            
+        <View style={styles.container}>
+            <ImageComponent
+                source={image_url ?? images.product_placeholder}
+                style={styles.image}
+            />
+            <Text style={styles.title}>{name}</Text>
         </View>
     );
 }
--- frontend/src/components/molecules/product-card/styles.ts
+++ frontend/src/components/molecules/product-card/styles.ts
@@ -1,5 +1,19 @@
 import { StyleSheet } from "react-native";
+import { fontFamilies } from "../../../utils/fonts";
 
 export const styles = StyleSheet.create({
-    
+    title: {
+        color: 'black',
+        fontSize: 15,
+        fontFamily: fontFamilies.satoshi.medium,
+        textAlign: 'center'
+    },
+    image: {
+        width: '100%',
+        height: 200,
+    },
+    container: {
+        flex: 1,
+        margin: 5
+    }
 });
