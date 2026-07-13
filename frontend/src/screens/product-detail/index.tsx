254012f75c66e9c20622e9bf014e7209b9e77ee5
cristians-12 <cristian.sanchez.casd@gmail.com>
2026-07-12 21:01:26 -0500

    feat: add route handling to ProductDetailScreen for parameter extraction

--- frontend/src/screens/product-detail/index.tsx
+++ frontend/src/screens/product-detail/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ProductDetailTemplate } from '../../templates';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { RootStackParamList } from '../../types/navigation';
+
+type ProductDetailRouteProp = RouteProp<RootStackParamList, 'ProductDetail'>;
 
 export default function ProductDetailScreen() {
+
+    const route = useRoute<ProductDetailRouteProp>();
+    const { id } = route.params;
+
     return (
         <SafeAreaView>
             <ProductDetailTemplate />
