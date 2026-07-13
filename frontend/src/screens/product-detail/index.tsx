8a4beed2193851c2bb6ec063f2bb9fbc18db01e6
cristians-12 <cristian.sanchez.casd@gmail.com>
2026-07-12 20:53:00 -0500

    feat: add ProductDetailTemplate and CartTemplate components; update imports and styles

--- frontend/src/screens/product-detail/index.tsx
+++ frontend/src/screens/product-detail/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ProductDetailTemplate } from '../../templates';
 
 export default function ProductDetailScreen() {
     return (
         <SafeAreaView>
-
+            <ProductDetailTemplate />
         </SafeAreaView>
     );
 }
--- frontend/src/templates/index.ts
+++ frontend/src/templates/index.ts
@@ -1,4 +1,4 @@
-export { default as ProductDetail } from './product-detail';
+export { default as ProductDetailTemplate } from './product-detail';
 export { default as HomeTemplate } from './home';
 export { default as CartTemplate } from './cart';
 export { default as SplashAnimation } from './splash-animation';
