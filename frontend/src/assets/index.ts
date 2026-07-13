c6273beedb16f9da4e8eb28a0fc041aa8dae3c76
cristians-12 <cristian.sanchez.casd@gmail.com>
2026-07-13 17:37:18 -0500

    refactor: replace PNG logos with WEBP format and update image references

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,6 +1,6 @@
 export const images = {
     logo: require('./images/logo.png'),
-    product_placeholder: require('./images/product_placeholder.png'),
-    mastercard_logo:require('./images/mastercard_logo.png'),
-    visa_logo:require('./images/visa_logo.png'),
+    product_placeholder: require('./images/product_placeholder.webp'),
+    mastercard_logo:require('./images/mastercard_logo.webp'),
+    visa_logo:require('./images/visa_logo.webp'),
 }
