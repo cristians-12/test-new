e0b448a9f1c8a4090d4f77a4409001877a2ed85e
cristians-12 <cristian.sanchez.casd@gmail.com>
2026-07-13 12:47:16 -0500

    feat: add Mastercard and Visa logos to asset images

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,4 +1,6 @@
 export const images = {
     logo: require('./images/logo.png'),
     product_placeholder: require('./images/product_placeholder.png'),
+    mastercard_logo:require('./images/mastercard_logo.png'),
+    visa_logo:require('./images/visa_logo.png'),
 }
